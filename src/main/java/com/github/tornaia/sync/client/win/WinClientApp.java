4c119d3a5b07392aed412e59db8c6d6b7636851d
Tornai András, PMK-NBI-SWD <Andras.Tornai@swisscom.com>
2016-10-01 14:44:23 +0200

    Initial commit

---
+++ src/main/java/com/github/tornaia/sync/client/win/WinClientApp.java
@@ -0,0 +1,96 @@
+package com.github.tornaia.sync.client.win;
+
+import com.sun.nio.file.SensitivityWatchEventModifier;
+import org.apache.commons.io.IOUtils;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+import java.io.IOException;
+import java.lang.management.ManagementFactory;
+import java.nio.charset.Charset;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.concurrent.TimeUnit;
+
+import static java.nio.file.StandardWatchEventKinds.*;
+
+public class WinClientApp {
+
+    private static final String SERVER_URL = "http://127.0.0.1:8080";
+    private static final String HELLO_PATH = "/api/hello";
+
+    public static void main(String[] args) throws IOException {
+        Path tempDirectory = FileSystems.getDefault().getPath("C:\\temp\\");
+        System.out.println("PID: " + ManagementFactory.getRuntimeMXBean().getName());
+        String serverInfo = getServerInfo();
+        System.out.println("Server says: " + serverInfo);
+
+        try (WatchService watcher = FileSystems.getDefault().newWatchService()) {
+            tempDirectory.register(watcher, new WatchEvent.Kind[]{ENTRY_CREATE, ENTRY_DELETE, ENTRY_MODIFY, OVERFLOW}, SensitivityWatchEventModifier.HIGH);
+            registerRecursive(watcher, tempDirectory);
+
+            while (true) {
+                WatchKey key;
+                try {
+                    key = watcher.poll(25, TimeUnit.MILLISECONDS);
+                } catch (InterruptedException e) {
+                    return;
+                }
+                if (key == null) {
+                    Thread.yield();
+                    continue;
+                }
+
+                for (WatchEvent<?> event : key.pollEvents()) {
+                    WatchEvent.Kind<?> kind = event.kind();
+                    WatchEvent<Path> ev = (WatchEvent<Path>) event;
+                    Path filename = ev.context();
+                    System.out.println(kind.name() + ", filename: " + filename);
+
+                    if (kind == OVERFLOW) {
+                        continue;
+                    }
+                    if (kind == ENTRY_CREATE) {
+                        onDirectoryChange(filename);
+                    } else if (kind == ENTRY_DELETE) {
+                        onDirectoryChange(filename);
+                    } else if (kind == ENTRY_MODIFY) {
+                        onDirectoryChange(filename);
+                    }
+
+                    boolean valid = key.reset();
+                    if (!valid) {
+                        break;
+                    }
+                }
+                Thread.yield();
+            }
+        } catch (Throwable e) {
+            // Log or rethrow the error
+        }
+    }
+
+    private static void registerRecursive(WatchService watchService, Path root) throws IOException {
+        Files.walkFileTree(root, new SimpleFileVisitor<Path>() {
+            @Override
+            public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+                System.out.println("WatchService registered for dir: " + dir.getFileName());
+                dir.register(watchService, ENTRY_CREATE, ENTRY_DELETE, ENTRY_MODIFY);
+                return FileVisitResult.CONTINUE;
+            }
+        });
+    }
+
+    private static HttpClient httpClient = HttpClientBuilder.create().build();
+
+    private static void onDirectoryChange(Path filename) {
+        // TODO
+    }
+
+    private static String getServerInfo() throws IOException {
+        HttpResponse response = httpClient.execute(new HttpGet(SERVER_URL + HELLO_PATH));
+        return IOUtils.toString(response.getEntity().getContent(), Charset.defaultCharset());
+    }
+}
