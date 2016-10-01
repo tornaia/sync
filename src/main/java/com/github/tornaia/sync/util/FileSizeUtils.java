a60495c09ba59a405901c9f175c4dfd08a9f3049
Tornai András, PMK-NBI-SWD <Andras.Tornai@swisscom.com>
2016-10-01 16:11:13 +0200

    Print file size in a pretty way

---
+++ src/main/java/com/github/tornaia/sync/util/FileSizeUtils.java
@@ -0,0 +1,24 @@
+package com.github.tornaia.sync.util;
+
+import java.text.DecimalFormat;
+
+public final class FileSizeUtils {
+
+    private static final String[] UNITS = new String[]{"B", "kB", "MB", "GB", "TB"};
+
+    private FileSizeUtils() {
+    }
+
+    public static String toReadableFileSize(long size) {
+        if (size < 0) {
+            throw new IllegalArgumentException("Size must not be negative: " + size);
+        }
+
+        if (size == 0) {
+            return "0";
+        }
+
+        int digitGroups = (int) (Math.log10(size) / Math.log10(1024));
+        return new DecimalFormat("#,##0.#").format(size / Math.pow(1024, digitGroups)) + " " + UNITS[digitGroups];
+    }
+}
