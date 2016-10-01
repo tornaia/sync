e88a053d15ecd36394c64bfbd1f51f8825ca5f8a
Tornai András, PMK-NBI-SWD <Andras.Tornai@swisscom.com>
2016-10-01 15:18:20 +0200

    Depend on shared project (api)

--- src/main/java/com/github/tornaia/sync/server/file/AddFileRequest.java
+++
@@ -1,7 +0,0 @@
-package com.github.tornaia.sync.server.file;
-
-/**
- * Created by TAATOAN3 on 10/1/2016.
- */
-public class AddFileRequest {
-}
--- src/main/java/com/github/tornaia/sync/server/file/FileController.java
+++ src/main/java/com/github/tornaia/sync/server/file/FileController.java
@@ -1,17 +1,20 @@
-package com.github.tornaia.sync.server.hello;
+package com.github.tornaia.sync.server.file;
 
+import com.github.tornaia.sync.shared.AddFileRequest;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.atomic.AtomicInteger;
 
 @RestController
-public class HelloController {
+public class FileController {
 
     private AtomicInteger counter = new AtomicInteger();
 
-    @RequestMapping("/api/hello")
-    public HelloResponse hello() {
-        return new HelloResponse("Hello " + counter.getAndIncrement());
+    @RequestMapping(value = "/api/file", method = RequestMethod.PUT)
+    public void addFile(@RequestBody AddFileRequest addFileRequest) {
+        return;
     }
 }
