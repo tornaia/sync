1ce5de953e96c7c78a9c7605e956b1d9cf14ceb1
Tornai András, PMK-NBI-SWD <Andras.Tornai@swisscom.com>
2016-10-01 20:58:03 +0200

    Sync really big files too (until a more proper solution is implemented)

--- src/main/java/com/github/tornaia/sync/server/CustomizationBean.java
+++ src/main/java/com/github/tornaia/sync/server/CustomizationBean.java
@@ -3,13 +3,17 @@ package com.github.tornaia.sync.server;
 import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
 import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
+
+import javax.servlet.MultipartConfigElement;
 
 @Component
 public class CustomizationBean implements EmbeddedServletContainerCustomizer {
 
     private static final int _1GB = 1073741824;
+    public static final String _1024MB = "1024MB";
 
     @Override
     public void customize(ConfigurableEmbeddedServletContainer container) {
@@ -28,5 +32,13 @@ public class CustomizationBean implements EmbeddedServletContainerCustomizer {
                 tomcat.addConnectorCustomizers((connector) -> connector.setMaxPostSize(_1GB));
             }
         };
+    }
+
+    @Bean
+    public MultipartConfigElement multipartConfigElement() {
+        MultipartConfigFactory factory = new MultipartConfigFactory();
+        factory.setMaxFileSize(_1024MB);
+        factory.setMaxRequestSize(_1024MB);
+        return factory.createMultipartConfig();
     }
 }
