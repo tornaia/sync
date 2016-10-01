f17994978f8148fdf3d8cbd40e0061db9d4d5aa7
Tornai András, PMK-NBI-SWD <Andras.Tornai@swisscom.com>
2016-10-01 19:11:56 +0200

    Fix Json mapping

--- src/main/java/com/github/tornaia/sync/server/controller/FolderController.java
+++ src/main/java/com/github/tornaia/sync/server/controller/FolderController.java
@@ -2,10 +2,7 @@ package com.github.tornaia.sync.server.controller;
 
 import javax.annotation.Resource;
 
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.github.tornaia.sync.server.data.document.Folder;
 import com.github.tornaia.sync.server.data.repository.FolderRepository;
@@ -18,12 +15,11 @@ public class FolderController {
 
 	@RequestMapping(value = "/folders/{id}", method = RequestMethod.GET, produces = "application/json", consumes = "application/json")
 	public Folder getFolder(@PathVariable("id") String id) {
-
 		return folderRepository.findOne(id);
 	}
 
-	@RequestMapping(value = "/folders", method = RequestMethod.POST) //, produces = "application/json", consumes = "application/json"
-	public Folder postFolder(Folder folder) {
+	@RequestMapping(value = "/folders", method = RequestMethod.POST)
+	public Folder postFolder(@RequestBody Folder folder) {
 		Folder savedFolder = folderRepository.insert(folder);
 		return savedFolder;
 	}
