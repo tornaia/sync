98a4fe7400f57c1f2671fe256102142dec80a245
Tornai András, PMK-NBI-SWD <Andras.Tornai@swisscom.com>
2016-10-01 17:04:13 +0200

    Minor cleanup

--- src/main/java/com/github/tornaia/sync/server/file/FileController.java
+++ src/main/java/com/github/tornaia/sync/server/file/FileController.java
@@ -1,22 +1,25 @@
 package com.github.tornaia.sync.server.file;
 
-import com.github.tornaia.sync.util.FileSizeUtils;
+import com.github.tornaia.sync.shared.api.PutDirectoryRequest;
+import com.github.tornaia.sync.shared.util.FileSizeUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.util.concurrent.atomic.AtomicInteger;
 
 @RestController
 public class FileController {
 
-    private AtomicInteger counter = new AtomicInteger();
-
     @RequestMapping(value = "/api/file", method = RequestMethod.PUT)
-    public void addFile(@RequestParam("userid") String userid, @RequestPart("file") MultipartFile multipartFile) throws IOException {
+    public void putFile(@RequestParam("userid") String userid, @RequestPart("file") MultipartFile multipartFile) throws IOException {
         String relativePathWithinSyncDir = multipartFile.getOriginalFilename();
         byte[] bytes = multipartFile.getBytes();
-        System.out.println("File received: " + relativePathWithinSyncDir + " (" + FileSizeUtils.toReadableFileSize(bytes.length) + ")");
-        return;
+        System.out.println("PUT file: " + relativePathWithinSyncDir + " (" + FileSizeUtils.toReadableFileSize(bytes.length) + ")");
+    }
+
+    @RequestMapping(value = "/api/directory", method = RequestMethod.PUT)
+    public void putDirectory(@RequestParam("userid") String userid, @RequestBody PutDirectoryRequest putDirectoryRequest) throws IOException {
+        String relativePath = putDirectoryRequest.relativePath;
+        System.out.println("PUT directory: " + relativePath);
     }
 }
