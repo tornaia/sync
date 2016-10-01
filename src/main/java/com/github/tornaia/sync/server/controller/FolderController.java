a6f7e562bb46866427ddac818966467250796d6e
Tornai András, PMK-NBI-SWD <Andras.Tornai@swisscom.com>
2016-10-01 19:15:12 +0200

    Cleanup

--- src/main/java/com/github/tornaia/sync/server/controller/FolderController.java
+++ src/main/java/com/github/tornaia/sync/server/controller/FolderController.java
@@ -1,27 +1,25 @@
 package com.github.tornaia.sync.server.controller;
 
-import javax.annotation.Resource;
-
-import org.springframework.web.bind.annotation.*;
-
 import com.github.tornaia.sync.server.data.document.Folder;
 import com.github.tornaia.sync.server.data.repository.FolderRepository;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
 
 @RestController
 public class FolderController {
 
-	@Resource
-	private FolderRepository folderRepository;
+    @Resource
+    private FolderRepository folderRepository;
 
-	@RequestMapping(value = "/folders/{id}", method = RequestMethod.GET, produces = "application/json", consumes = "application/json")
-	public Folder getFolder(@PathVariable("id") String id) {
-		return folderRepository.findOne(id);
-	}
+    @RequestMapping(value = "/folders/{id}", method = RequestMethod.GET)
+    public Folder getFolder(@PathVariable("id") String id) {
+        return folderRepository.findOne(id);
+    }
 
-	@RequestMapping(value = "/folders", method = RequestMethod.POST)
-	public Folder postFolder(@RequestBody Folder folder) {
-		Folder savedFolder = folderRepository.insert(folder);
-		return savedFolder;
-	}
-
+    @RequestMapping(value = "/folders", method = RequestMethod.POST)
+    public Folder postFolder(@RequestBody Folder folder) {
+        Folder savedFolder = folderRepository.insert(folder);
+        return savedFolder;
+    }
 }
