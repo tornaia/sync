04a4dfecf063b013964f1015d2feccf2c50fa95c
pubi2006 <pubi2006@gmail.com>
2016-10-02 16:13:18 +0200

    Cleanup unused Classes

--- src/main/java/com/github/tornaia/sync/server/data/repository/RequestRepository.java
+++
@@ -1,9 +0,0 @@
-package com.github.tornaia.sync.server.data.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.github.tornaia.sync.server.data.document.Request;
-
-public interface RequestRepository extends MongoRepository<Request, String> {
-
-}
