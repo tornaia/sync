b53f2128756537636c6d9395c0399275365ca780
Tornai András, PMK-NBI-SWD <Andras.Tornai@swisscom.com>
2016-10-02 17:37:41 +0200

    Cleanup

--- src/main/java/com/github/tornaia/sync/client/win/httpclient/FailOnErrorResponseInterceptor.java
+++ src/main/java/com/github/tornaia/sync/client/win/httpclient/FailOnErrorResponseInterceptor.java
@@ -5,13 +5,12 @@ import org.apache.http.HttpResponseInterceptor;
 import org.apache.http.HttpStatus;
 import org.apache.http.protocol.HttpContext;
 
-import java.io.IOException;
 import java.util.Objects;
 
 public class FailOnErrorResponseInterceptor implements HttpResponseInterceptor {
 
     @Override
-    public void process(HttpResponse response, HttpContext context) throws IOException {
+    public void process(HttpResponse response, HttpContext context) {
         int statusCode = response.getStatusLine().getStatusCode();
         if (!Objects.equals(statusCode, HttpStatus.SC_OK)) {
             throw new IllegalStateException("Non-OK response: " + response);
