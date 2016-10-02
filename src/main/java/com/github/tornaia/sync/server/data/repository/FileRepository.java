009905c0cc293a58758eb8521f3d381279c4920d
Tornai András, PMK-NBI-SWD <Andras.Tornai@swisscom.com>
2016-10-02 21:40:24 +0200

    MongoDB query bugfix

--- src/main/java/com/github/tornaia/sync/server/data/repository/FileRepository.java
+++ src/main/java/com/github/tornaia/sync/server/data/repository/FileRepository.java
@@ -1,22 +1,19 @@
 package com.github.tornaia.sync.server.data.repository;
 
-import java.util.List;
-
+import com.github.tornaia.sync.server.data.document.File;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
-import org.springframework.data.repository.query.Param;
 
-import com.github.tornaia.sync.server.data.document.File;
+import java.util.List;
 
 public interface FileRepository extends MongoRepository<File, String> {
 
-	@Query("{'path' : ?0}")
-	File findByPath(String path);
-	
-	@Query("{'id' : ?0, 'userId' : ?1}")
-	File findByIdAndUserId(String path, String userId);
-	
-	@Query("SELECT * FROM File f where f.userId = :userId and f.lastModifiedDate > :lastModifiedDate")
-	List<File> findAllModified(@Param("lastModifiedDate")Long lastModifiedDate,@Param("userId") String userId);
+    @Query("{'path' : ?0}")
+    File findByPath(String path);
 
+    @Query("{'id' : ?0, 'userId' : ?1}")
+    File findByIdAndUserId(String path, String userId);
+
+    @Query("{'userId' : ?0}, 'lastModifiedDate' : {$gt : ?1}")
+    List<File> findAllModified(String userId, long lastModifiedDate);
 }
