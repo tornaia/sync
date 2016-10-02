d51f555b5c879d10a11352f08b619d45cf913c1f
Tornai András, PMK-NBI-SWD <Andras.Tornai@swisscom.com>
2016-10-02 17:32:56 +0200

    Minor

--- src/main/java/com/github/tornaia/sync/shared/api/FileMetaInfo.java
+++ src/main/java/com/github/tornaia/sync/shared/api/FileMetaInfo.java
@@ -8,38 +8,43 @@ import java.nio.file.attribute.BasicFileAttributes;
 
 public class FileMetaInfo implements Serializable {
 
-	public final String id;
+    public final String id;
     public final String relativePath;
     public final long length;
     public final long creationDateTime;
     public final long modificationDateTime;
 
     public FileMetaInfo() {
-    	this.id = null;
+        this.id = null;
         this.relativePath = null;
         this.length = -1;
         this.creationDateTime = -1;
         this.modificationDateTime = -1;
     }
 
-    public FileMetaInfo(String id, String relativePath, File file) throws IOException {
-    	this.id = id;
+    public static FileMetaInfo createNonSyncedFileMetaInfo(String relativePath, File file) {
+        return new FileMetaInfo(null, relativePath, file);
+    }
+
+    public FileMetaInfo(String id, String relativePath, File file) {
+        this.id = id;
         this.relativePath = relativePath;
-        BasicFileAttributes attr = Files.readAttributes(file.toPath(), BasicFileAttributes.class);
+        BasicFileAttributes attr = null;
+        try {
+            attr = Files.readAttributes(file.toPath(), BasicFileAttributes.class);
+        } catch (IOException e) {
+            throw new RuntimeException("Cannot create FileMetaInfo", e);
+        }
         this.length = attr.size();
         this.creationDateTime = attr.creationTime().toMillis();
         this.modificationDateTime = attr.lastModifiedTime().toMillis();
     }
 
-	public FileMetaInfo(String id, String relativePath, long length, long creationDateTime, long modificationDateTime) {
-		super();
-		this.id = id;
-		this.relativePath = relativePath;
-		this.length = length;
-		this.creationDateTime = creationDateTime;
-		this.modificationDateTime = modificationDateTime;
-	}
-    
-    
-
+    public FileMetaInfo(String id, String relativePath, long length, long creationDateTime, long modificationDateTime) {
+        this.id = id;
+        this.relativePath = relativePath;
+        this.length = length;
+        this.creationDateTime = creationDateTime;
+        this.modificationDateTime = modificationDateTime;
+    }
 }
