ce41b075811e35d3f5d1f5c8e2ca84256eb76325
nxjohny <sallaidaniel@gmail.com>
2016-10-03 19:33:46 +0200

    SpringbootApplication stereotype annotation contains Componentscan

--- src/main/java/com/github/tornaia/sync/server/ServerApp.java
+++ src/main/java/com/github/tornaia/sync/server/ServerApp.java
@@ -1,14 +1,12 @@
 package com.github.tornaia.sync.server;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 
 import static org.springframework.boot.SpringApplication.run;
 
 @EnableWebSocket
 @SpringBootApplication
-@ComponentScan("com.github.tornaia.sync.server")
 public class ServerApp {
 
     public static void main(String[] args) throws Exception {
