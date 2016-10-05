4a9c627b458c277a746b5a0e4a461836a620b1f7
Tornai András, PMK-NBI-SWD <Andras.Tornai@swisscom.com>
2016-10-05 20:17:56 +0200

    Make first test green

--- src/test/java/com/github/tornaia/sync/e2e/WinClientAndServerIntTests.java
+++ src/test/java/com/github/tornaia/sync/e2e/WinClientAndServerIntTests.java
@@ -1,20 +1,29 @@
 package com.github.tornaia.sync.e2e;
 
 import com.github.tornaia.sync.client.win.WinClientApp;
+import com.github.tornaia.sync.client.win.httpclient.FailOnErrorResponseInterceptor;
 import com.github.tornaia.sync.server.ServerApp;
 import org.apache.commons.io.IOUtils;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.assertj.core.util.Files;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileWriter;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
@@ -28,7 +37,7 @@ public class WinClientAndServerIntTests {
     // later this tests should run on jenkins, other op systems or
     // we should be able to run our test suit against target environments
 
-    private static final long userId = System.currentTimeMillis();
+    private static final long userid = System.currentTimeMillis();
 
     private static final String client1SyncDirectoryPath = "C:\\temp\\client-1\\";
     private static final String client1StateFilePath = "C:\\temp2\\client-1.db";
@@ -36,8 +45,18 @@ public class WinClientAndServerIntTests {
     private static final String client2SyncDirectoryPath = "C:\\temp\\client-2\\";
     private static final String client2StateFilePath = "C:\\temp2\\client-2.db";
 
+
+    @Value("#{systemProperties['server.scheme.http'] ?: 'http'}")
+    private String serverSchemeHttp;
+
+    @Value("#{systemProperties['server.host'] ?: '127.0.0.1'}")
+    private String serverHost;
+
+    @Value("#{systemProperties['server.port'] ?: '8080'}")
+    private int serverPort;
+
     @Before
-    public void initServerWith2Clients() {
+    public void initServerWith2Clients() throws Exception {
         Files.delete(new File(client1SyncDirectoryPath));
         Files.delete(new File(client2SyncDirectoryPath));
         Files.delete(new File(client1StateFilePath));
@@ -45,30 +64,65 @@ public class WinClientAndServerIntTests {
 
         new SpringApplicationBuilder(ServerApp.class).headless(false).run();
 
+        resetDB();
+
         new SpringApplicationBuilder(WinClientApp.class)
                 .web(false)
                 .headless(false)
                 .run("--client.sync.directory.path=" + client1SyncDirectoryPath,
                         "--client.state.file.path=" + client1StateFilePath,
-                        "--frosch-sync.user.id=" + userId);
+                        "--frosch-sync.userid=" + userid);
 
         new SpringApplicationBuilder(WinClientApp.class)
                 .web(false)
                 .headless(false)
                 .run("--client.sync.directory.path=" + client2SyncDirectoryPath,
                         "--client.state.file.path=" + client2StateFilePath,
-                        "--frosch-sync.user.id=" + userId);
+                        "--frosch-sync.userid=" + userid);
+
+
+    }
+
+    private void resetDB() throws Exception {
+        Thread thread = new Thread(() ->
+        {
+            try {
+                HttpClient httpClient = HttpClientBuilder.
+                        create()
+                        .disableAutomaticRetries()
+                        .addInterceptorFirst(new FailOnErrorResponseInterceptor())
+                        .build();
+
+                URI uri = new URIBuilder()
+                        .setScheme(serverSchemeHttp)
+                        .setHost(serverHost)
+                        .setPort(serverPort)
+                        .setPath("/api/files/reset")
+                        .build();
+
+                HttpGet httpGet = new HttpGet(uri);
+
+                httpClient.execute(httpGet);
+                LOG.info("DB reset done");
+            } catch (URISyntaxException | IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        thread.start();
+        thread.join();
     }
 
     // TODO super ugly I know but I want to get a failing (then passing) test ASAP
     @Test
     public void bothCanStart() throws Exception {
+        Thread.sleep(1000L);
+
         assertTrue(new File(client1SyncDirectoryPath).list().length == 0);
 
-        IOUtils.write("dummy content", new FileWriter(new File(client1SyncDirectoryPath + "/dummy.txt")));
+        IOUtils.write("dummy content", new FileOutputStream(client1SyncDirectoryPath + "/dummy.txt"));
 
         // wait for sync
-        Thread.sleep(1000L);
+        Thread.sleep(5000L);
 
         assertTrue(new File(client1SyncDirectoryPath).list().length == 1);
         assertTrue(new File(client2SyncDirectoryPath).list().length == 1);
