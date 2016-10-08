6e361ec5683ffce5a9eb18ec14a82ca495e9a2a9
Tornai András, PMK-NBI-SWD <Andras.Tornai@swisscom.com>
2016-10-08 12:35:09 +0200

    Comment update

--- src/test/java/com/github/tornaia/sync/server/controller/FileControllerTest.java
+++ src/test/java/com/github/tornaia/sync/server/controller/FileControllerTest.java
@@ -2,9 +2,7 @@ package com.github.tornaia.sync.server.controller;
 
 import com.github.tornaia.sync.server.service.FileCommandService;
 import com.github.tornaia.sync.server.service.FileQueryService;
-import com.github.tornaia.sync.server.service.exception.FileAlreadyExistsException;
 import com.github.tornaia.sync.server.service.exception.FileNotFoundException;
-import com.github.tornaia.sync.shared.api.CreateFileRequest;
 import com.github.tornaia.sync.shared.api.GetFileMetaInfoRequest;
 import com.github.tornaia.sync.shared.api.GetFileRequest;
 import com.google.gson.Gson;
@@ -13,17 +11,11 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
-import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import static org.mockito.Matchers.*;
-import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.doThrow;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.fileUpload;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -40,7 +32,7 @@ public class FileControllerTest {
     @Autowired
     private MockMvc mvc;
 
-    // TODO a bútorosok épp jönnek.. nem tul profi.
+    // TODO https://github.com/springfox/springfox/issues/588
 //    @Test
 //    public void createFile() throws Exception {
 //        MockMultipartFile file = new MockMultipartFile("file", "file.png", "image/png", "test".getBytes());
