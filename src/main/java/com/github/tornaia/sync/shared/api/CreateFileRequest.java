8865166a4acc0ecb0839d6fc39e2350b8af37275
Tornai András, PMK-NBI-SWD <Andras.Tornai@swisscom.com>
2016-10-08 08:48:00 +0200

    Code formatting

--- src/main/java/com/github/tornaia/sync/shared/api/CreateFileRequest.java
+++ src/main/java/com/github/tornaia/sync/shared/api/CreateFileRequest.java
@@ -1,13 +1,11 @@
 package com.github.tornaia.sync.shared.api;
 
-/**
- * Created by Bence on 07-Oct-16.
- */
 public class CreateFileRequest extends FileModificationRequest {
 
-    public CreateFileRequest(){}
+    public CreateFileRequest() {
+    }
 
-    public CreateFileRequest(String userId, long creationDateTime, long modificationDateTime){
+    public CreateFileRequest(String userId, long creationDateTime, long modificationDateTime) {
         this.userId = userId;
         this.creationDateTime = creationDateTime;
         this.modificationDateTime = modificationDateTime;
