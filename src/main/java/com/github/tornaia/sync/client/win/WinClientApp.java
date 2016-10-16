b4e83c99ed338ea6f4b6051d346af24aeda91499
Tornai András, PMK-NBI-SWD <Andras.Tornai@swisscom.com>
2016-10-16 12:12:07 +0200

    Add explicit component scan

--- src/main/java/com/github/tornaia/sync/client/win/WinClientApp.java
+++ src/main/java/com/github/tornaia/sync/client/win/WinClientApp.java
@@ -4,10 +4,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.ComponentScan;
 
 import java.lang.management.ManagementFactory;
 
 @SpringBootApplication
+@ComponentScan("com.github.tornaia.sync")
 public class WinClientApp {
 
     private static final Logger LOG = LoggerFactory.getLogger(WinClientApp.class);
