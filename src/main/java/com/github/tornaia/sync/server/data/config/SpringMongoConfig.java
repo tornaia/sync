dcb74aa5c8e76bce738cd83306d3376232d5bbb2
Tornai András, PMK-NBI-SWD <Andras.Tornai@swisscom.com>
2016-10-16 13:28:35 +0200

    Refactor

--- src/main/java/com/github/tornaia/sync/server/data/config/SpringMongoConfig.java
+++ src/main/java/com/github/tornaia/sync/server/data/config/SpringMongoConfig.java
@@ -1,12 +1,14 @@
 package com.github.tornaia.sync.server.data.config;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.tornaia.sync.shared.util.SerializerUtils;
 import com.mongodb.Mongo;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
 import com.mongodb.client.MongoDatabase;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
@@ -21,42 +23,49 @@ public class SpringMongoConfig extends AbstractMongoConfiguration {
 
     private static final Logger LOG = LoggerFactory.getLogger(SpringMongoConfig.class);
 
-    // TODO use @Value
-    private static final String VCAP_SERVICES = "VCAP_SERVICES";
+    @Value("${VCAP_SERVICES:#{null}}")
+    private String vcapServices;
 
-    // TODO use @Value
-    private String HOST = "localhost";
-    private int PORT = 27017;
-    private String DATABASE_NAME = "sync-database";
+    @Value("${mongo.reset.on.start:false}")
+    private boolean resetOnStart;
 
-    private String COLLECTION_NAME = "sync-collection";
+    @Value("${mongo.local.development.host:localhost}")
+    private String host;
 
-    // TODO use @Value
-    private boolean resetDbOnStart = false;
+    @Value("${mongo.local.development.port:27017}")
+    private int port;
+
+    @Value("${mongo.local.development.database.name:sync-database}")
+    private String databaseName;
+
+    @Value("${mongo.local.development.collection.name:sync-collection}")
+    private String collectionName;
+
+    @Autowired
+    private SerializerUtils serializerUtils;
 
     @Override
     protected String getDatabaseName() {
-        return DATABASE_NAME;
+        return databaseName;
     }
-
 
     @Bean
     @Override
     public Mongo mongo() throws Exception {
-        LOG.info("Reset db on start? " + resetDbOnStart);
-        if (!Objects.isNull(System.getenv(VCAP_SERVICES))) {
-            LOG.info("We are in cloud! Init mongo for cloud!");
+        LOG.info("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX vcapServices: " + vcapServices);
+        boolean isCloud = !Objects.isNull(vcapServices);
+        if (isCloud) {
             return initCloud();
         } else {
-            LOG.info("Local development. Init mongo for local!");
             return initLocal();
         }
     }
 
     private MongoClient initCloud() throws IOException {
-        LOG.info("initCloud");
-        String vcapServicesAsJson = System.getenv(VCAP_SERVICES);
-        Map<String, Object> vcapServicesMap = parse(vcapServicesAsJson);
+        LOG.info("We are in cloud. Init Mongo for Cloud!");
+        LOG.info("Reset on start? " + resetOnStart);
+        LOG.trace("vcapServices: " + vcapServices);
+        Map<String, Object> vcapServicesMap = serializerUtils.toObject(vcapServices, Map.class);
         List<Map<String, Object>> mongoDbList = (List<Map<String, Object>>) vcapServicesMap.get("mongodb");
         Map<String, Object> mongoDbFirstMap = mongoDbList.get(0);
         Map<String, String> credentialsMap = (Map<String, String>) mongoDbFirstMap.get("credentials");
@@ -64,30 +73,28 @@ public class SpringMongoConfig extends AbstractMongoConfiguration {
         String database = credentialsMap.get("database");
         MongoClient mongoClient = new MongoClient(new MongoClientURI(database_uri));
         MongoDatabase mongoDatabase = mongoClient.getDatabase(database);
-        if (resetDbOnStart) {
+        if (resetOnStart) {
             mongoDatabase.drop();
         }
         mongoDatabase = mongoClient.getDatabase(database);
-        if (resetDbOnStart) {
-            mongoDatabase.createCollection(COLLECTION_NAME);
+        if (resetOnStart) {
+            mongoDatabase.createCollection(collectionName);
         }
         return mongoClient;
     }
 
     private MongoClient initLocal() {
-        MongoClient mongoClient = new MongoClient(HOST, PORT);
-        MongoDatabase mongoDatabase = mongoClient.getDatabase(DATABASE_NAME);
-        if (resetDbOnStart) {
+        LOG.info("Local development. Init Mongo for Local!");
+        LOG.info("Reset on start? " + resetOnStart);
+        MongoClient mongoClient = new MongoClient(host, port);
+        MongoDatabase mongoDatabase = mongoClient.getDatabase(databaseName);
+        if (resetOnStart) {
             mongoDatabase.drop();
         }
-        mongoDatabase = mongoClient.getDatabase(DATABASE_NAME);
-        if (resetDbOnStart) {
-            mongoDatabase.createCollection(COLLECTION_NAME);
+        mongoDatabase = mongoClient.getDatabase(databaseName);
+        if (resetOnStart) {
+            mongoDatabase.createCollection(collectionName);
         }
         return mongoClient;
-    }
-
-    private Map<String, Object> parse(String json) throws IOException {
-        return new ObjectMapper().readValue(json, Map.class);
     }
 }
