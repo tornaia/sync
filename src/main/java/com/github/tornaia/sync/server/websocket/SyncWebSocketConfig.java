295244f53fc4c6248bd0a929c56b4d2bf83375ac
Tornai András, PMK-NBI-SWD <Andras.Tornai@swisscom.com>
2016-10-20 23:00:02 +0200

    Replace hardcoded value of websocket path with a configurable one

--- src/main/java/com/github/tornaia/sync/server/websocket/SyncWebSocketConfig.java
+++ src/main/java/com/github/tornaia/sync/server/websocket/SyncWebSocketConfig.java
@@ -1,6 +1,7 @@
 package com.github.tornaia.sync.server.websocket;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
@@ -8,11 +9,14 @@ import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
 @Configuration
 public class SyncWebSocketConfig implements WebSocketConfigurer {
 
+    @Value("${sync.pusher.server.path}")
+    private String webSocketPath;
+
     @Autowired
     private SyncWebSocketHandler syncWebSocketHandler;
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(syncWebSocketHandler, "/echo").setAllowedOrigins("*");
+        registry.addHandler(syncWebSocketHandler, webSocketPath).setAllowedOrigins("*");
     }
 }
