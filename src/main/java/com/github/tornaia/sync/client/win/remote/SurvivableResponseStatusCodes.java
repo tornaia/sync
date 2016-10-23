ac488a5f5a45b336e6fa6db108a5486ea25099a3
Tornai András, PMK-NBI-SWD <Andras.Tornai@swisscom.com>
2016-10-23 14:37:41 +0200

    Sometimes a valid request ends with a 406 (NOT_ACCEPTABLE) however everything was valid

--- src/main/java/com/github/tornaia/sync/client/win/remote/SurvivableResponseStatusCodes.java
+++ src/main/java/com/github/tornaia/sync/client/win/remote/SurvivableResponseStatusCodes.java
@@ -11,7 +11,7 @@ import static java.util.Arrays.asList;
 public final class SurvivableResponseStatusCodes {
 
     private static final List<Integer> BACKEND_DEPLOYMENT_ONGOING = asList(HttpStatus.SC_INTERNAL_SERVER_ERROR, HttpStatus.SC_BAD_GATEWAY);
-    private static final List<Integer> TEMPORARY_NETWORK_PROBLEM = asList(HttpStatus.SC_BAD_GATEWAY);
+    private static final List<Integer> TEMPORARY_NETWORK_PROBLEM = asList(HttpStatus.SC_BAD_GATEWAY, HttpStatus.SC_NOT_ACCEPTABLE);
 
     public static final Set<Integer> SOLVABLE_BY_REPEAT_STATUS_CODES;
 
