40491209bf31ddcb52212c2ff6bb8c2e4ca008dd
Tornai András, PMK-NBI-SWD <Andras.Tornai@swisscom.com>
2016-10-23 08:41:53 +0200

    Refactor of constants

---
+++ src/main/java/com/github/tornaia/sync/shared/constant/FileSizeConstants.java
@@ -0,0 +1,11 @@
+package com.github.tornaia.sync.shared.constant;
+
+public final class FileSizeConstants {
+
+    // TODO these values should be respected by the client too
+    public static final int MAX_SYNC_FILE_SIZE_IN_BYTES = 5 * 1073741824;
+    public static final String MAX_SYNC_FILE_SIZE_IN_MEGABYTES = "5120MB";
+
+    private FileSizeConstants() {
+    }
+}
