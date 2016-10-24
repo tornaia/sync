baa660e0d885dbba33c3d2c4a0a1ddc66675813c
Tornai András, PMK-NBI-SWD <Andras.Tornai@swisscom.com>
2016-10-24 20:37:42 +0200

    Add DeleteFileRequest class When user wants to delete a file he must send not just the id but the other attributes too to ensure he will delete the proper version file from the server

---
+++ src/main/java/com/github/tornaia/sync/shared/api/DeleteFileRequest.java
@@ -0,0 +1,41 @@
+package com.github.tornaia.sync.shared.api;
+
+public class DeleteFileRequest extends AbstractRequest {
+
+    private String id;
+    private long size;
+    private long creationDateTime;
+    private long modificationDateTime;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public long getSize() {
+        return size;
+    }
+
+    public void setSize(long size) {
+        this.size = size;
+    }
+
+    public long getCreationDateTime() {
+        return creationDateTime;
+    }
+
+    public void setCreationDateTime(Long creationDateTime) {
+        this.creationDateTime = creationDateTime;
+    }
+
+    public long getModificationDateTime() {
+        return modificationDateTime;
+    }
+
+    public void setModificationDateTime(Long modificationDateTime) {
+        this.modificationDateTime = modificationDateTime;
+    }
+}
---
+++ src/main/java/com/github/tornaia/sync/shared/api/DeleteFileRequestBuilder.java
@@ -0,0 +1,38 @@
+package com.github.tornaia.sync.shared.api;
+
+public class DeleteFileRequestBuilder {
+
+    private String id;
+    private Long size;
+    private Long creationDateTime;
+    private Long modificationDateTime;
+
+    public DeleteFileRequest create() {
+        DeleteFileRequest deleteFileRequest = new DeleteFileRequest();
+        deleteFileRequest.setId(id);
+        deleteFileRequest.setSize(size);
+        deleteFileRequest.setCreationDateTime(creationDateTime);
+        deleteFileRequest.setModificationDateTime(modificationDateTime);
+        return deleteFileRequest;
+    }
+
+    public DeleteFileRequestBuilder id(String id) {
+        this.id = id;
+        return this;
+    }
+
+    public DeleteFileRequestBuilder size(long size) {
+        this.size = size;
+        return this;
+    }
+
+    public DeleteFileRequestBuilder creationDateTime(long creationDateTime) {
+        this.creationDateTime = creationDateTime;
+        return this;
+    }
+
+    public DeleteFileRequestBuilder modificationDateTime(long modificationDateTime) {
+        this.modificationDateTime = modificationDateTime;
+        return this;
+    }
+}
