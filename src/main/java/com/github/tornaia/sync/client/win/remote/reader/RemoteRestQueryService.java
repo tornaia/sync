85c3b37c54d467442910cafd660a555fe67433f7
Tornai András, PMK-NBI-SWD <Andras.Tornai@swisscom.com>
2016-10-26 00:08:46 +0200

    Add FIXME

--- src/main/java/com/github/tornaia/sync/client/win/remote/reader/RemoteRestQueryService.java
+++ src/main/java/com/github/tornaia/sync/client/win/remote/reader/RemoteRestQueryService.java
@@ -3,7 +3,10 @@ package com.github.tornaia.sync.client.win.remote.reader;
 import com.github.tornaia.sync.client.win.httpclient.HttpClientProvider;
 import com.github.tornaia.sync.shared.api.FileMetaInfo;
 import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.NotImplementedException;
 import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.util.EntityUtils;
 import org.slf4j.Logger;
@@ -13,6 +16,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.util.Objects;
 
 @Component
 public class RemoteRestQueryService {
@@ -31,18 +35,25 @@ public class RemoteRestQueryService {
     public byte[] getFile(FileMetaInfo fileMetaInfo) {
         HttpGet httpGet = new HttpGet(httpClientProvider.getServerUrl() + backendFileApiPath + "/" + fileMetaInfo.id + "?userid=" + userid);
 
-        byte[] response;
+        byte[] content;
         try {
-            HttpEntity entity = httpClientProvider.get().execute(httpGet).getEntity();
-            // FIXME here sometimes we get 406 (0 content length is causing this)
-            // FIXME here we have a memory limitation: redesign to use inputstream instead of byte[]. 5GB file failed here with OOM
-            response = IOUtils.toByteArray(entity.getContent());
+            HttpResponse response = httpClientProvider.get().execute(httpGet);
+            HttpEntity entity = response.getEntity();
+            content = IOUtils.toByteArray(entity.getContent());
+            boolean ok = Objects.equals(response.getStatusLine().getStatusCode(), HttpStatus.SC_OK);
+
+            if (!ok) {
+                EntityUtils.consume(entity);
+                // FIXME introduce FileGetResponse (like FileDeleteResponse,FileCreateResponse,FileModifyResponse)...
+                throw new NotImplementedException("When servers is not available or something goes wrong (here) then the logic will break");
+            }
+            // TODO here we have a memory limitation: redesign to use inputStream instead of byte[]. 5GB file failed here with OOM
             EntityUtils.consume(entity);
         } catch (IOException e) {
             throw new RuntimeException("Get from server failed", e);
         }
 
         LOG.debug("GET file: " + fileMetaInfo);
-        return response;
+        return content;
     }
 }
