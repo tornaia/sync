5fdfe983a33b55d01d5b36b7fa36e5e6b96ca3a8
Tornai András, PMK-NBI-SWD <Andras.Tornai@swisscom.com>
2016-10-27 00:10:19 +0200

    Introduce DynamicStorageException If there is a communication problem with the dynamic storage (and sometimes it happens even if this time is probably my fault somehow) then we must not propegate back a 406 (NOT_ACCEPTABLE) response to the client. So far the client expected some kind of application/octet-stream but he got a text response (with the stack trace, etc) -> 406. From now he will get 500 (Internal Server Error) and he will (should) retry later.

--- src/main/java/com/github/tornaia/sync/client/win/remote/SurvivableResponseStatusCodes.java
+++ src/main/java/com/github/tornaia/sync/client/win/remote/SurvivableResponseStatusCodes.java
@@ -2,27 +2,16 @@ package com.github.tornaia.sync.client.win.remote;
 
 import org.apache.http.HttpStatus;
 
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 import static java.util.Arrays.asList;
 
 public final class SurvivableResponseStatusCodes {
 
-    private static final List<Integer> BACKEND_DEPLOYMENT_ONGOING = asList(HttpStatus.SC_INTERNAL_SERVER_ERROR, HttpStatus.SC_BAD_GATEWAY);
-    private static final List<Integer> TEMPORARY_NETWORK_PROBLEM = asList(HttpStatus.SC_BAD_GATEWAY, HttpStatus.SC_NOT_ACCEPTABLE);
-
-    public static final Set<Integer> SOLVABLE_BY_REPEAT_STATUS_CODES;
-
-    static {
-        SOLVABLE_BY_REPEAT_STATUS_CODES = new HashSet<>();
-        SOLVABLE_BY_REPEAT_STATUS_CODES.addAll(BACKEND_DEPLOYMENT_ONGOING);
-        SOLVABLE_BY_REPEAT_STATUS_CODES.addAll(TEMPORARY_NETWORK_PROBLEM);
-    }
+    private static final List<Integer> TEMPORARY_NETWORK_PROBLEM = asList(HttpStatus.SC_BAD_GATEWAY);
 
     public static boolean isSolvableByRepeat(int statusCode) {
-        return SOLVABLE_BY_REPEAT_STATUS_CODES.contains(statusCode);
+        return TEMPORARY_NETWORK_PROBLEM.contains(statusCode);
     }
 
     private SurvivableResponseStatusCodes() {
